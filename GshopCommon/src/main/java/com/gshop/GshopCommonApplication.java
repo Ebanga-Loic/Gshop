3fb7ddb4819ed12e03fd0e3a9ef36e2f55d8a4fd
Ebanga Loic <ebangaarnaud@gmail.com>
2024-06-13 19:59:14 +0100

    Création de la bibliothèque commune

---
+++ GshopCommon/src/main/java/com/gshop/GshopCommonApplication.java
@@ -0,0 +1,13 @@
+package com.gshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GshopCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GshopCommonApplication.class, args);
+	}
+
+}
---
+++ GshopCommon/src/test/java/com/gshop/GshopCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GshopCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
